d9edeb27e930d557cd0c58c8705491593be06299
Supun Sanjana <infor.ssupun@gmail.com>
2025-08-24 23:29:40 +0530

    create user registration controller and route

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from 'express';
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoute from './src/router/userRoute.js'
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("✅ Connected to MongoDB");
+})
+    .catch((err) => {
+        console.error("❌ Error connecting to MongoDB:", err);
+    });
+
+
 const PORT = process.env.SERVER_PORT
 
-app.listen(PORT,(req, res)=>{
+app.use("/api/v1", userRoute)
+
+app.listen(PORT, (req, res) => {
     console.log(`server is running on port ${PORT}`);
 
 })
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,39 @@
+import userSchema from '../model/user.js'
+import bcrypt from "bcryptjs";
+
+
 //register user
-const register = async ()=>{
-    
+export const register = async (req, res) => {
+
+    try {
+        const { firstName, lastName, userName, email, password } = req.body
+
+        if (!firstName || !userName || !email || !password) {
+            res.status(400).json({
+                success: false,
+                message: "Missing required fields"
+            })
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = bcrypt.hashSync(password, salt);
+
+        const user = new userSchema({
+            firstName,
+            lastName,
+            userName,
+            email,
+            password: hashPassword
+        })
+
+        const savaData = await user.save()
+        res.status(201).json({
+                success: true,
+                message: "User saved success !"
+            })
+    } catch (err) {
+        console.log(err);
+
+    }
+
 }
